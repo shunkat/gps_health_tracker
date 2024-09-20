fb7d71e925d0933ddb8fb7b9474aec1b2dbde5a4
shunkat <shunkat@icloud.com>
2024-09-20 17:53:29 +0900

    設定ページからもアンケートダイアログを呼べるように変更

--- lib/presentation/setting/setting_page.dart
+++ lib/presentation/setting/setting_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gps_health_tracker/presentation/widgets/survey_dialog.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
@@ -7,15 +8,19 @@ class SettingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('設定'),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              '設定ページ',
+            const Text('設定ページ'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                showSurveyDialog(context);
+              },
+              child: const Text('アンケート再回答'),
             ),
           ],
         ),
