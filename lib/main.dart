1223c05396b3a152e120254992fc9a02ad6d4117
shunkat <shunkat@icloud.com>
2024-10-12 16:25:20 +0900

    初期表示画面を認証状態によって条件分岐するような仕組みにmain.dartを変更 - 認証済み→ホーム画面 - 未認証→認証画面

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gps_health_tracker/presentation/auth/auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:gps_health_tracker/presentation/home/home_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -14,19 +16,29 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      // TODO: 認証状態に応じて表示するページを変える
-      home: const AuthPage(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: '認知症研究用アプリ',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+            if (snapshot.hasData) {
+              return const HomePage();
+            }
+            return const AuthPage();
+          },
+        )
+      );
 }
-
 
